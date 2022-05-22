0be2572ef2a12248a78244cb3b9d3dfa1272cace
Manishpndn131 <67330336+noobie131@users.noreply.github.com>
2022-05-22 11:20:48 +0000

    Fixed bugs

--- Prob10JSB.js
+++ Prob10JSB.js
@@ -1,18 +1,3 @@
-/*
-var n = [3,9,1,44,6];
-
-n.sort(function(a, b) {
-  for(i=0;i<n.length;i++)
-  {
-  if(n[i]%2 == 0)
-   { return a - b;}
-  }
-});
-
-console.log(n);
-
-*/
-
 function Numeric_sort(ar) {
      
     for(i=0;i<ar.length;i++){
